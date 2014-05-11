9ec57def5dae5a9e03acf4696458f96644965a21
XuHaoJun <XuHaoJunTW@gmail.com>
2014-05-11 23:11:00 +0800

    Update.

--- plugins/tag_cloud.rb
+++ plugins/tag_cloud.rb
@@ -2,46 +2,46 @@
 
 # Tag Cloud for Octopress
 # =======================
-# 
+#
 # Description:
 # ------------
 # Easy output tag cloud and category list.
-# 
+#
 # Syntax:
 # -------
 #     {% tag_cloud [counter:true] %}
 #     {% category_list [counter:true] %}
-# 
+#
 # Example:
 # --------
 # In some template files, you can add the following markups.
-# 
+#
 # ### source/_includes/custom/asides/tag_cloud.html ###
-# 
+#
 #     <section>
 #       <h1>Tag Cloud</h1>
 #         <span id="tag-cloud">{% tag_cloud %}</span>
 #     </section>
-# 
+#
 # ### source/_includes/custom/asides/category_list.html ###
-# 
+#
 #     <section>
 #       <h1>Categories</h1>
 #         <ul id="category-list">{% category_list counter:true %}</ul>
 #     </section>
-# 
+#
 # Notes:
 # ------
 # Be sure to insert above template files into `default_asides` array in `_config.yml`.
 # And also you can define styles for 'tag-cloud' or 'category-list' in a `.scss` file.
 # (ex: `sass/custom/_styles.scss`)
-# 
+#
 # Licence:
 # --------
 # Distributed under the [MIT License][MIT].
-# 
+#
 # [MIT]: http://www.opensource.org/licenses/mit-license.php
-# 
+#
 require 'stringex'
 
 module Jekyll
@@ -99,7 +99,7 @@ module Jekyll
       config = context.registers[:site].config
       category_dir = config['category_dir']
       categories = context.registers[:site].categories
-      categories.keys.sort_by{ |str| str.downcase }.each do |category|
+      categories.keys.sort_by{ |str| str.downcase }.sort{ |a,b| categories[b].count <=> categories[a].count }.each do |category|
         html << "<a class='list-group-item' href='/#{category_dir}/#{category.to_url}/'>"
         if @opts['counter']
           html << "<span class='badge'> #{categories[category].count} </span>"
